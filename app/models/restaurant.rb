c01537760e9d9e76a6baeeeb02f277afa72d4735
Octavian Serban <octa_serban@yahoo.com>
2021-08-10 14:07:51 +0400

    yelp mvp exercise commit restaurant model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORY}
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "000000" , category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "000001" , category: "italian" }
+
+[ dishoom, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
